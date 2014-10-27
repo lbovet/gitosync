3db4f1fa5a21c8bc02ffa8e26a22fb84861c14e8
lbovet <laurent.bovet@windmaster.ch>
2014-10-28 00:08:51 +0100

    Implemented REST API

--- app.rb
+++ app.rb
@@ -1,21 +1,24 @@
 # Require the bundler gem and then call Bundler.require to load in all gems
 # listed in Gemfile.
 require 'bundler'
+require './sync'
 Bundler.require
 
-# Setup DataMapper with a database URL. On Heroku, ENV['DATABASE_URL'] will be
-# set, when working locally this line will fall back to using SQLite in the
-# current directory.
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite")
+DataMapper::Logger.new(STDOUT, :debug)
 
-# Define a simple DataMapper model.
-class Thing
+# Setup DataMapper with a database URL.
+DataMapper.setup(:default, "sqlite://#{Dir.pwd}/data/db.sqlite")
+
+# DataMapper model.
+class Sync
   include DataMapper::Resource
-
   property :id, Serial, :key => true
-  property :created_at, DateTime
-  property :title, String, :length => 255
-  property :description, Text
+  property :space, String, :length => 10..255, :required => true
+  property :name, String, :length => 255, :required => true
+  property :from, String, :length => 255, :required => true
+  property :to, String, :length => 255, :required => true
+  property :status, Enum[ :IDLE, :SCHEDULED, :RUNNING ], :default => :IDLE
+  property :log, Text
 end
 
 # Finalize the DataMapper models.
@@ -28,82 +31,96 @@ get '/' do
   send_file './public/index.html'
 end
 
-# Route to show all Things, ordered like a blog
-get '/things' do
+# Route to show all syncs of a space
+get '/spaces/:space/syncs/' do
   content_type :json
-  @things = Thing.all(:order => :created_at.desc)
-
-  @things.to_json
+  @syncs = Sync.all(:space => params[:space], :order => :name).map{|sync| sync.name}
+  @syncs.to_json
 end
 
-# CREATE: Route to create a new Thing
-post '/things' do
+# READ: Route to show a specific Sync
+get '/spaces/:space/syncs/:name' do
   content_type :json
-
-  # These next commented lines are for if you are using Backbone.js
-  # JSON is sent in the body of the http request. We need to parse the body
-  # from a string into JSON
-  # params_json = JSON.parse(request.body.read)
-
-  # If you are using jQuery's ajax functions, the data goes through in the
-  # params.
-  @thing = Thing.new(params)
-
-  if @thing.save
-    @thing.to_json
-  else
-    halt 500
-  end
-end
-
-# READ: Route to show a specific Thing based on its `id`
-get '/things/:id' do
-  content_type :json
-  @thing = Thing.get(params[:id].to_i)
-
-  if @thing
-    @thing.to_json
+  @sync = Sync.first(:space => params[:space], :name => params[:name])
+  if @sync
+    @sync.to_json(:exclude => [:id, :space, :name])
   else
     halt 404
   end
 end
 
-# UPDATE: Route to update a Thing
-put '/things/:id' do
+# UPDATE: Route to create or update a Sync
+put '/spaces/:space/syncs/:name' do
   content_type :json
+  @input = JSON.parse(request.body.read)
+  @input.delete("id")
+  @input.delete("space")
+  @input.delete("name")
+  @input.delete("status")
+  begin
+    @sync = Sync.first_or_create({:space => params[:space], :name => params[:name]}, @input)  
+    if not @sync.new?
+      @sync.update(@input)
+    end
+    if @sync.valid?
+      if @sync.save
+        @sync.to_json(:exclude => [:id, :space, :name])
+      else
+        halt 500      
+      end
+    else 
+      body @sync.errors.full_messages.join("\n")   
+      halt 400
+    end
+  rescue ArgumentError => e
+        body e.message
+        halt 400      
+  end    
+end
 
-  # These next commented lines are for if you are using Backbone.js
-  # JSON is sent in the body of the http request. We need to parse the body
-  # from a string into JSON
-  # params_json = JSON.parse(request.body.read)
-
-  # If you are using jQuery's ajax functions, the data goes through in the
-  # params.
-
-  @thing = Thing.get(params[:id].to_i)
-  @thing.update(params)
-
-  if @thing.save
-    @thing.to_json
+# RUN: Schedule a Sync for execution
+post '/spaces/:space/syncs/:name' do
+  content_type :json
+  @sync = Sync.first(:space => params[:space], :name => params[:name])
+  if @sync
+    if @sync.status == :IDLE
+      @sync.status = :SCHEDULED
+      if @sync.save
+        ''
+      else
+        halt 500
+      end
+    else
+      body "Cannot schedule because already scheduled or running"
+      halt 409
+    end
   else
-    halt 500
+    halt 404
   end
 end
 
-# DELETE: Route to delete a Thing
-delete '/things/:id/delete' do
+# DELETE: Route to delete a Sync
+delete '/spaces/:space/syncs/:name' do
   content_type :json
-  @thing = Thing.get(params[:id].to_i)
-
-  if @thing.destroy
-    {:success => "ok"}.to_json
+  @sync = Sync.first(:space => params[:space], :name => params[:name])
+  if @sync
+    if @sync.destroy
+      ''
+    else
+      halt 500
+    end
   else
-    halt 500
+    halt 404
   end
 end
 
-# If there are no Things in the database, add a few.
-if Thing.count == 0
-  Thing.create(:title => "Test Thing One", :description => "Sometimes I eat pizza.")
-  Thing.create(:title => "Test Thing Two", :description => "Other times I eat cookies.")
+# Reset status on startup
+Sync.all(:status.not => :IDLE).map do |sync|
+  sync.status = :IDLE
+  sync.save
+end
+
+if Sync.count == 0
+  Sync.create(:space => "1000", :name => "hello")
+  Sync.create(:space => "1000", :name => "world", :status => :RUNNING)
 end
